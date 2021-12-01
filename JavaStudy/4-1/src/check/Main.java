8979f6c8331c3991769f1a7fbd1f72dd3da165a4
daisukegogo <g031n029@gmail.com>
2021-12-01 13:18:26 +0900

    Update Main.java

--- JavaStudy/4-1/src/check/Main.java
+++ JavaStudy/4-1/src/check/Main.java
@@ -4,16 +4,16 @@ package check;
 import constants.Constants;
 
 public class Main {
-	// フィールド変数
-	private String firstName = "及川";
-	private String lastName = "大輔";
+	// フィールド変数(static付与により自クラスから直接アクセス)
+	private static String firstName = "及川";
+	private static String lastName = "大輔";
 	
 	//自クラスのメソッドを使うためのmainメソッド
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		Main a = new Main();//オブジェクトの生成
-		a.printName(a.firstName,a.lastName); //フィールド変数・メソッドへアクセス
+		printName(firstName,lastName); //自クラスのフィールド変数・メソッドへアクセス
+		
 		//インスタンス化(①クラス名②インスタンス名（変数名）③= new④コンストラクタ名)
 		// importしたクラスフィールドを使用
 		Pet pt = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE); 
@@ -23,7 +23,9 @@ public class Main {
 		rpt.introduce();
 	}
 	
-	private void printName(String fName,String lName){
+	//(static付与により自クラスから直接アクセス)
+	private static void printName(String fName,String lName){
+	
 		System.out.println("printNameメソッド→" + fName + lName);
 	}
 }
